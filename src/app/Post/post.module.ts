cc7ed20bd7f0e741e56a7d2520d01c723526a4b9
jramma <jrmasip97@gmail.com>
2024-12-01 13:27:02 +0100

    post form

--- src/app/Post/post.module.ts
+++ src/app/Post/post.module.ts
@@ -1,6 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 import { FormatDatePipe } from '../Shared/Pipes/format-date.pipe';
 import { HomeComponent } from './components/home/home.component';
 import { PostFormComponent } from './components/post-form/post-form.component';
@@ -15,6 +23,16 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
     FormatDatePipe,
     DashboardComponent,
   ],
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule, // Para los contenedores de entrada de texto
+    MatInputModule, // Para los inputs
+    MatButtonModule, // Para los botones
+    MatSelectModule, // Para los selectores de categorías
+    MatDatepickerModule, // Para el selector de fechas
+    MatNativeDateModule, // Necesario para que funcione MatDatepicker
+    MatProgressSpinnerModule, // Para mostrar el spinner de carga
+  ],
 })
 export class PostModule {}
